1cc8d6fef443648b127576154bc4b5f96594ee68
eugenie coisne <coisne.eugenie@gmail.com>
2020-05-28 15:38:50 +0200

    models all created with associations & validation

---
+++ db/migrate/20200528102902_create_places.rb
@@ -0,0 +1,19 @@
+class CreatePlaces < ActiveRecord::Migration[6.0]
+  def change
+    create_table :places do |t|
+      t.string :name
+      t.string :address
+      t.string :zip_code
+      t.string :city
+      t.string :phone_number
+      t.string :email
+      t.boolean :ephemeral
+      t.string :siret_number
+      t.string :website
+      t.string :instagram
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
